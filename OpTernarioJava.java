73e98896e12fc3315bfe3374259acee70fadc49a
Fabricio Curvello <109672685+fabcurvello@users.noreply.github.com>
2024-05-05 21:04:52 -0300

    Classe sobre Operador Ternário Java

---
+++ OpTernarioJava.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class OpTernarioJava {
+    public static void main(String[] args) {
+        //Operador ternário em Java:
+        //<Expressão Condicional> ? <Caso condição seja true> : <Caso condição seja false>
+
+        int a = 1, b = 2;
+        String resultado = a > b ? "Verdadeiro" : "Falso" ; // Operador ternário
+
+        System.out.println(resultado);
+    }
+}

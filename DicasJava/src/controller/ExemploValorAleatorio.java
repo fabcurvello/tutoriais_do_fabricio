b91ddda6527df870cba90dfdffa2449106db7bc9
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-05-11 22:37:04 -0300

    Atualizando Valor Aleatório Java

--- DicasJava/src/controller/ExemploValorAleatorio.java
+++ DicasJava/src/controller/ExemploValorAleatorio.java
@@ -1,16 +1,26 @@
 package controller;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+
+import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
 
 public class ExemploValorAleatorio {
 
     public static void main(String[] args) {
-        
 
-        // dois números: origem e fim; um número: fim. Considerar até o anterior ao último
-        System.out.println(ThreadLocalRandom.current().nextInt(10, 21));
-
+        //PARA AMBIENTES SINGLE-THREADS (básicos)
+        Random random = new Random();
+        int sorteio1 = random.nextInt(10, 21); // entre 10 e 20
         // existe .nextInt(), .nextDouble(), dentre diversos outros.
+        System.out.println("Sorteio1: " + sorteio1);
+        
+        //ADEQUADO PARA AMBIENTES MULTITHREADS
+        // dois números: origem e fim; um número: fim. Considerar até o anterior ao último
+        int sorteio2 = ThreadLocalRandom.current().nextInt(10, 21);
+        // existe .nextInt(), .nextDouble(), dentre diversos outros.
+        System.out.println("Sorteio2: " + sorteio2);
+
     }
 
 }
